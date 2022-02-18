b6a1d6f6db207c7bea3697546e8135b5fd83306e
izure <izure@naver.com>
2022-02-18 17:53:01 +0900

    fix types

--- packages/core/types/core/src/CodraftRunner.d.ts
+++ packages/core/types/core/src/CodraftRunner.d.ts
@@ -1,4 +1,4 @@
-import { Codraft, MacroDataTransfer, SupportedVariableType } from "../../@typings/codraft";
+import { Codraft, MacroDataTransfer } from "../../@typings/codraft";
 export declare class CodraftRunner {
     private __events;
     private __conditions;
@@ -10,8 +10,8 @@ export declare class CodraftRunner {
     protected static CreateHashMap<T extends {
         id: string;
     }>(sources: T[]): Map<string, T>;
-    protected static ParseVariables(command: Codraft.MacroCommand, variables: Record<string, string>, local: Record<string, SupportedVariableType>, global: Record<string, SupportedVariableType>): Record<string, SupportedVariableType>;
-    protected static ParseVariable(command: Codraft.MacroCommand, variables: Record<string, string>, key: string, local: Record<string, SupportedVariableType>, global: Record<string, SupportedVariableType>): SupportedVariableType;
+    protected static ParseVariables(command: Codraft.MacroCommand, variables: Record<string, string>, local: Record<string, any>, global: Record<string, any>): Record<string, any>;
+    protected static ParseVariable(command: Codraft.MacroCommand, variables: Record<string, string>, key: string, local: Record<string, any>, global: Record<string, any>): any;
     protected static RunCommand(command: Codraft.MacroCommand, variables: Record<string, string>, dataTransfer: MacroDataTransfer): Promise<MacroDataTransfer>;
     protected static RunCommandAsync(command: Codraft.MacroCommand, variables: Record<string, string>, dataTransfer: MacroDataTransfer, resolve: (data: MacroDataTransfer) => void, reject: (reason?: Error) => void): void;
     protected static FindBox(boxes: Codraft.MacroBox[], id: string): Codraft.MacroBox | null;
@@ -19,7 +19,7 @@ export declare class CodraftRunner {
     protected __runCommand(format: Codraft.MacroCommandSaveFormat, data: MacroDataTransfer): Promise<MacroDataTransfer>;
     protected __runCommandAsync(format: Codraft.MacroCommandSaveFormat, data: MacroDataTransfer, resolve: (data: MacroDataTransfer) => void, reject: (reason?: Error) => void): void;
     protected __runCommands(formats: Codraft.MacroCommandSaveFormat[], data: MacroDataTransfer): Promise<void>;
-    protected __recursiveBox(box: Codraft.MacroBox, data: MacroDataTransfer): void;
+    protected __recursiveBox(box: Codraft.MacroBox, data: MacroDataTransfer): Promise<void>;
     protected __attachEvent(): void;
     init(): void;
     setDebugMode(isDebug: boolean): void;
--- packages/core/types/core/src/CodraftSolver.d.ts
+++ packages/core/types/core/src/CodraftSolver.d.ts
@@ -3,13 +3,13 @@ export declare class CodraftSolver {
     static SolveString(v: string): string;
     static SolveNumber(v: string): number;
     static SolveBoolean(v: string): boolean;
-    static SolveDynamic(v: string): SupportedVariableType;
+    static SolveDynamic(v: string): any;
     static readonly VariableSolver: {
         string: (v: string) => string;
         number: (v: string) => number;
         boolean: (v: string) => boolean;
-        dynamic: (v: string) => SupportedVariableType;
+        dynamic: (v: string) => any;
     };
     static GetVariableType(command: Codraft.MacroCommand, key: string): RawVariable<SupportedVariableType>['type'] | null;
-    static GetVariableSolver(type: ReturnType<typeof CodraftSolver.GetVariableType>): ((v: string) => SupportedVariableType) | null;
+    static GetVariableSolver(type: ReturnType<typeof CodraftSolver.GetVariableType>): ((v: string) => any) | null;
 }
